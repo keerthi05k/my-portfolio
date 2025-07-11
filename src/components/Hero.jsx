143f4a27165a5e5d1b52c236dbb24285456edd26
keerthanakannan-7 <keerthanakannan3833@gmail.com>
2025-07-11 16:42:24 +0530

    Updated Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,13 +8,13 @@ export const Hero = () => {
   }
   return (
     <section id="hero" className="flex flex-col md:flex-row px-5 py-32 bg-primary justify-center">
-        <div className="md:w-1/2 flex flex-col">
+        <div className="md:w-1/2 flex flex-col mt-6">
              <h1 className="w-1/2 text-white text-6xl font-hero-font">Hi, <br /> I'm <span className="text-black">Keerthana</span></h1>
-             <p className="text-4xl text-white">{config.subtitle}</p>
+             <p className="text-4xl text-white mt-2">{config.subtitle}</p>
          
              <div className="flex gap-4 py-5">
-                <a href="https://www.linkedin.com/in/keerthana-kannan05/" className="text-white"><CiLinkedin size={42} /></a>
-                <a href="https://github.com/keerthi05k" className="text-white"><FaGithub size={38} /></a>
+                <a target="_blank" href="https://www.linkedin.com/in/keerthana-kannan05/" className="text-white"><CiLinkedin size={42} /></a>
+                <a target="_blank" href="https://github.com/keerthi05k" className="text-white"><FaGithub size={38} /></a>
              </div>
         </div>
         <img className="md:w-1/3" src={HeroImg} alt="HeroImg"/>

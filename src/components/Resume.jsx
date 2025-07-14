75d34586a0f5a773edb3aa779d7f1219328b093b
keerthanakannan-7 <keerthanakannan3833@gmail.com>
2025-07-14 08:23:59 +0530

    Updated heading text in App.js

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -10,7 +10,7 @@ export const Resume = () => {
             <div className="md:w-1/2 flex justify-center">
                 <div className="flex flex-col justify-center text-white">
                     <h1 className="text-4xl border-b-4 border-[#5353da] mb-5 w-[135px] font-bold">Resume</h1>
-                    <p className="text-justify pb-5">You Can View My Resume <a target="_blank" className="btn" href="https://drive.google.com/drive/folders/1a7YNysV9KzAPKdivs5GNd0j1Oadd1Cys">Download</a></p>
+                    <p className="text-justify pb-5">You Can View My Resume <a target="_blank" className="btn" href="https://drive.google.com/file/d/1t1-pg8PmP5LiI4Dm95366j_T5w03_YGa/view?usp=drive_link">Download</a></p>
                 </div>
             </div>
         </section>
